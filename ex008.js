9972f338d7d26643042b3e8a3f989e7c5693bc4f
Thais Ayume <thais8ayume@gmail.com>
2023-09-05 17:15:46 -0300

    Novos exercícios

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60 ) {
+    console.log ('você ultrapassou a velocidade permitida, você está multado!!')
+}
+console.log('dirija com cuidado utilizando cinto de segurança')
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`vivendo em ${país}`)
+if (país == 'Brasil'){
+    console.log('brasileiro')
+}else {
+    console.log('Estrangeiro')
+}
