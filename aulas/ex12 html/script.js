1fe7662f4920c216768f9a8f8714a5dec71f0696
Thais Ayume <thais8ayume@gmail.com>
2023-09-12 11:19:41 -0300

    exercício 12

--- aulas/ex12 html/script.js
+++ aulas/ex12 html/script.js
@@ -1,1 +1,29 @@
-alert("olá")
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+
+var img = window.document.getElementById('imagem')
+
+var data = new Date()
+
+//var hora = data.getHours()
+
+var hora = 19
+
+msg.innerHTML = `Agora são ${hora} horas. `
+
+if (hora >= 0 && hora < 12){
+    //BOm dia
+    img.src = 'manhã.jpg'
+    document.body.style.background = '#6094B7'
+}else if (hora >= 12 && hora <= 18){
+    //boa tarde
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#F99D05'
+}else {
+    //boa noite
+    img.src = 'noite.jpg'
+    document.body.style.background = '#022953'
+}
+
+}
