542fce6cbb36dedb194c77c164e70dbdfedc86cb
Thais Ayume <thais8ayume@gmail.com>
2023-09-18 10:38:28 -0300

    término de exercícios

--- aulas/aula14ex/ex015/script.js
+++ aulas/aula14ex/ex015/script.js
@@ -14,22 +14,34 @@ function contar(){
     if (num1.value.length == 0 || num2.value.length == 0 || passo.value.length == 0){
         window.alert('[ERRO] Faltam Dados')
     }else{
-        result.innerHTML = 'contando: '
+        result.innerHTML = 'contando: <br> '
 
         var um = Number(num1.value)
         var dois = Number(num2.value)
         var p = Number(passo.value)
+
+        if (p <= 0){
+            window.alert('passo inválido! considerando passo 1 ')
+            p = 1
+        }
     
 
         //vai começar no início (num1) e enquanto "c" for menor ou igual ao (num2) o "c" vai receber ele mesmo mais o "passo"
-
-        for (var c = um; c <= dois; c += p){
-        result.innerHTML += ` ${c}`
+        if(um < dois){
+            //contagem crescente
+            for (var c = um; c <= dois; c += p){
+                result.innerHTML += ` ${c} \u{1F449}`
+                }
+              
+        }else {
+            //contagem regressiva
+            for (var c = um; c >= dois; c -= p  ){
+                result.innerHTML += `${c} \u{1F449}`
+            }
+            
         }
+        result.innerHTML += `\u{1F3C1}`
     }
-
-
-
 
 
 }
